37b14830b53fbc1c0ab8dfe2125947bf95cee6e1
Clawd <clawd@k-plus.one>
2026-01-25 02:44:01 +0000

    Initialize stack: Convex, Better Auth, Polar, Uploadthing

---
+++ convex/functions.ts
@@ -0,0 +1,12 @@
+import { query, mutation } from "./_generated/server";
+import { v } from "convex/values";
+
+export const getMyUser = query({
+  args: { email: v.string() },
+  handler: async (ctx, args) => {
+    return await ctx.db
+      .query("users")
+      .withIndex("by_email", (q) => q.eq("email", args.email))
+      .unique();
+  },
+});
